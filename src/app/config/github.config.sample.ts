b83b436e109745fc9cee647ab00bcbba3157dc3f
Edward Łuska <edi@nabi.pl>
2023-02-25 13:23:44 +0100

    Config cleanup

--- src/app/config/github.config.sample.ts
+++ src/app/config/github.config.sample.ts
@@ -4,8 +4,6 @@
 // For instruction on how to create personal access token visit:
 // https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
 
-// limit: fill the array with repo names or leave it empty and kill your browser with thousands of requests
-
 export const githubConfig = {
   url: 'https://api.github.com',
   username: 'YOUR_GITHUB_USERNAME',
@@ -14,5 +12,4 @@ export const githubConfig = {
   initPerPage: 100,
   reposPerPage: 30,
   contributorsPerPage: 30,
-  limit: ['zone.js', 'material.angular.io', 'router-builds', 'angular', 'material2', 'angular-cli', 'angular.js', 'material', 'protraktor'],
 };
